35d69d313ca36a5d808db79340fc1194710f6642
Blank <guoshaoxuan1@gmail.com>
2025-04-22 08:57:07 -0400

    iii

--- MyWorld.java
+++ MyWorld.java
@@ -3,5 +3,8 @@ import greenfoot.*;
 public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
+        
+        Car car = new Car();
+        addObject(car, 600, 100);
     }
 }
