733b31f78126dc0854804026e3ee77b80e2b3835
Pai0921 <440042698@gapps.yrdsb.ca>
2025-04-22 09:06:02 -0400

    team

--- MyWorld.java
+++ MyWorld.java
@@ -3,13 +3,12 @@ import greenfoot.*;
 public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
-        
-<<<<<<< HEAD
+    
         Hero hero = new Hero();
         addObject(hero, 100, 100);
-=======
+
         Car car = new Car();
         addObject(car, 600, 100);
->>>>>>> 35d69d313ca36a5d808db79340fc1194710f6642
+
     }
 }
